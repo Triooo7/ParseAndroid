2a95b0a19fb549a984675e6673079fc2d9242414
Krishna <krishna.raju93@gmail.com>
2018-12-12 07:30:04 +0100

    Initial commit

---
+++ app/src/main/java/com/compalk/parseapplicationserver/MainActivity.java
@@ -0,0 +1,15 @@
+package com.compalk.parseapplicationserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.compalk.parseapplicationserver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
